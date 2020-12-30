cf7b36175741bf68af3b9647b6e10118836690bb
gouri-panda <gouripanda4@gmail.com>
2020-12-30 13:49:58 +0530

    tested some animation project

--- app/src/main/java/com/example/testingproject/MainActivity.kt
+++ app/src/main/java/com/example/testingproject/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.testingproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.animation.AnimationUtils
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import dagger.Component
+
+const val TAG = "gouri"
+
+@Component
+interface CarFactory {
+    fun getCar(): Car
+    fun getWheel(): Wheel
+}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val car = DaggerCarFactory.create().getCar()
+        Log.d(TAG, "onCreate: car $car")
+        val wheel = DaggerCarFactory.create().getWheel()
+        Log.d(TAG, "onCreate: wheel $wheel")
+        val textView = findViewById<TextView>(R.id.textView)
+        Log.d(TAG, "onCreate: textViews layout Parameters ${textView.layoutParams}")
+        val constraintLayout = findViewById<ConstraintLayout>(R.id.constraint_layout)
+        Log.d(TAG, "onCreate: constraintLayout $constraintLayout")
     }
 }
