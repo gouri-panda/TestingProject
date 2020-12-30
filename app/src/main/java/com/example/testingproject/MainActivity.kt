a906efd32c6094d60aa0a9e5745726a013838a25
gouri-panda <gouripanda4@gmail.com>
2020-12-30 13:56:50 +0530

    added kotlin-android-extension

--- app/src/main/java/com/example/testingproject/MainActivity.kt
+++ app/src/main/java/com/example/testingproject/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.testingproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
