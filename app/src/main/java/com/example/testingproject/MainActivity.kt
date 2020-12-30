94d6a44e876a7632b0cb3804b4a62bc39cb14891
gouri-panda <gouripanda4@gmail.com>
2020-12-30 14:22:39 +0530

    build fixed

--- app/src/main/java/com/example/testingproject/MainActivity.kt
+++ app/src/main/java/com/example/testingproject/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.testingproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.flow
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
