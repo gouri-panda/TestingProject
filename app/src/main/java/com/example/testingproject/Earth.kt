a0e585e4996075111c3530319ac41bd11e2f7e94
gouri-panda <gouripanda4@gmail.com>
2021-02-14 15:21:45 +0530

    added commit

---
+++ app/src/main/java/com/example/testingproject/Earth.kt
@@ -0,0 +1,9 @@
+package com.example.testingproject
+
+import java.lang.reflect.Array.get
+
+class Earth {
+    fun rotate() {
+        //this is first version
+    }
+}
--- app/src/main/java/com/example/testingproject/MainActivity.kt
+++ app/src/main/java/com/example/testingproject/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.testingproject
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+private const val TAG = "mainActivity"
+
 class MainActivity : AppCompatActivity() {
+    //region start of MainActivity
+    private var _view: Int? = null
+    val view: Int
+        get() = _view!!
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        _view = 4
+        textView.setOnLongClickListener {
+            Log.d(TAG, "onCreate: log")
+            true
+        }
+        componentName
+
     }
 
+    fun foo(planetName: Any) {
+        if (planetName !is Earth) {
+            throw IllegalArgumentException()
+        }
+        planetName.rotate()
+    }
+    //endregion end of mainActivity
+
+
 }
+
