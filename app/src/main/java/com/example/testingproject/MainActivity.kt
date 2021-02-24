b16ddea269ccfa0e8fd954995d8c5baffcda8071
gouri-panda <gouripanda4@gmail.com>
2021-02-24 20:49:29 +0530

    added clock

--- app/src/main/java/com/example/testingproject/MainActivity.kt
+++ app/src/main/java/com/example/testingproject/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.example.testingproject
 
+import android.content.Context
+import android.graphics.*
+import android.os.Build
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        hello_world.canScrollVertically(-1)
+
+    }
+
+}
+
+class CanvasView @JvmOverloads constructor(
+     context: Context?,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+) :
+    View(context, attrs, defStyleAttr, defStyleRes) {
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        val paint = Paint().apply {
+            strokeCap = Paint.Cap.ROUND
+            style = Paint.Style.STROKE
+            strokeWidth = 10f
+            color = context?.getColor(R.color.white)!!
+        }
+        val rectF = RectF(0f, 0f, width.toFloat(), height.toFloat())
+        canvas?.drawCircle(width.toFloat() / 2, height.toFloat() / 2, width.toFloat() / 2, paint)
     }
 
 }
